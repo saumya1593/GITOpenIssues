1fd6e2f471c54ca2fa5afadb925edc80991b2dc7
saumya1593 <p15.saumya@gmail.com>
2019-02-27 23:35:49 +0530

    making changes in pythoncode.py

--- pythoncode.py
+++ pythoncode.py
@@ -53,41 +53,39 @@ def data():
 
 	json_content= json.loads(res.text)
 	total_open_issues = json_content['open_issues_count']
-
+	print(total_open_issues)
 	no_of_pages = math.ceil(total_open_issues/100)
 
 	api_url = "https://api.github.com/repos/" + user_id + "/" + repo + "/issues?page=<ff>&per_page=100"
 
-	total_issues = []
 	issues_opened_in_last_24 = []
 	issues_opened_in_1_to_7 = []
-	issues_opened_in_more_than_7 = []
 	for i in range(1,(no_of_pages+1)):
 	    url = re.sub("<ff>",str(i),api_url)
 	    response = requests.get(url,auth=HTTPBasicAuth('saumya1593','Zindagi@123'))
 	    issue_json_content= json.loads(response.text)
 	    for j in range(len(issue_json_content)):
 	        issue_dict = issue_json_content[j]
-	        total_issues.append(datetime.now(pytz.utc) - parse(issue_dict['created_at']))
 	        x = datetime.now(pytz.utc) - parse(issue_dict['created_at'])
 	        if x.days == 0:
 	            issues_opened_in_last_24.append(x)
 	        elif x.days > 0 and x.days <= 7:
 	            issues_opened_in_1_to_7.append(x)
 	        else:
-	            issues_opened_in_more_than_7.append(x)
-
-	total_issues = len(total_issues)
-	issues_opened_in_last_24 = len(issues_opened_in_last_24)
-	issues_opened_in_1_to_7 = len(issues_opened_in_1_to_7)
-	issues_opened_in_more_than_7 = len(issues_opened_in_more_than_7)   
+	            break
+	    
+	           
+	total = total_open_issues
+	total_issues_opened_in_last_24 = len(issues_opened_in_last_24)
+	total_issues_opened_in_1_to_7 = len(issues_opened_in_1_to_7) 
+	total_issues_opened_in_more_than_7 = total-(total_issues_opened_in_last_24+total_issues_opened_in_1_to_7)
 	        
 	# print(time.time()-st)
-	print(total_issues)
-	print(issues_opened_in_last_24)
-	print(issues_opened_in_1_to_7)
-	print(issues_opened_in_more_than_7) 
-	return render_template("data.html",total_issues=total_issues,issues_opened_in_last_24=issues_opened_in_last_24,issues_opened_in_1_to_7=issues_opened_in_1_to_7,issues_opened_in_more_than_7=issues_opened_in_more_than_7)
+	print(total)
+	print(total_issues_opened_in_last_24)
+	print(total_issues_opened_in_1_to_7)
+	print(total_issues_opened_in_more_than_7) 
+	return render_template("data.html",total_issues=total,issues_opened_in_last_24=total_issues_opened_in_last_24,issues_opened_in_1_to_7=total_issues_opened_in_1_to_7,issues_opened_in_more_than_7=total_issues_opened_in_more_than_7)
 	
 	
 if __name__ == "__main__":
